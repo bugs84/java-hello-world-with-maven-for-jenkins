69d954d8df29483a91c94993e0a9a09222315fc1
Jan Vondrouš <j.vondrous@quadient.com>
2022-11-04 10:43:37 +0100

    dep-management-version

--- dep-management-version/subproject-1/src/Main.java
+++ dep-management-version/subproject-1/src/Main.java
@@ -1,5 +1,9 @@
+import org.apache.commons.io.FilenameUtils;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        String concat = FilenameUtils.concat("base", "child");
+        System.out.println("FileNameUtils: Contcat = " + concat);
     }
 }
